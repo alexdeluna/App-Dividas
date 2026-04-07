ca6f2793d522dfaf7b39155bc64b3c23b15233e9
Alex de Luna <67716472+alexdeluna@users.noreply.github.com>
2026-04-07 19:59:28 -0300

    Add files via upload

---
+++ auth.js
@@ -0,0 +1,21 @@
+import { getAuth,
+         signInWithEmailAndPassword,
+         signOut,
+         onAuthStateChanged }
+from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
+
+import { app } from "./firebase-config.js";
+
+export const auth = getAuth(app);
+
+export function login(email, senha){
+  return signInWithEmailAndPassword(auth,email,senha);
+}
+
+export function logout(){
+  return signOut(auth);
+}
+
+export function observarSessao(callback){
+  onAuthStateChanged(auth,callback);
+}
